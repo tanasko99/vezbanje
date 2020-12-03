ed6bc08540688a338c87b0e0a9a11ea1ce7ca728
STANCO\Comp <stanic.veljko@gmail.com>
2020-12-03 16:19:10 +0100

    Kraj2

---
+++ PresentationLayer - Copy/DataAccessLayer/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string IndexNumber { get; set; }
+        public decimal AverageMark { get; set; }
+
+
+
+    }
+}
---
+++ dbo.TableStudents - Copy.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Students] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Name]        NVARCHAR (50)   NOT NULL,
+    [IndexNumber] NVARCHAR (50)   NOT NULL,
+    [AverageMark] DECIMAL (16, 2) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
